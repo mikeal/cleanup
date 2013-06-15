1a7043b323de1ad2058ddc568cf0321c64850802
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-06-15 14:07:42 -0700

    Always run on process exit, no matter what.

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var domain = require('domain')
 module.exports = function (cleanup, noOutput) {
   var d = domain.create()
   d.cleanup = once(cleanup)
-  d.on('error', function (e) {
+  var handler = once(function (e) {
+    if (e === 0 || !e) return d.cleanup()
     if (!noOutput) console.error(e.stack || e)
     d.cleanup(e)
   })
+
+  d.on('error', handler)
+  process.on('uncaughtException', handler)
+  process.on('exit', handler)
+
   d.enter()
   return d
 }
